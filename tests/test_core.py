8d53f8433b2f5a78f0b74bdf77cf09e80b6774c6
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-06-13 13:43:06 -0600

    test: Explicitly set no limit for test records (#294)

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,8 +2,12 @@
 
 from __future__ import annotations
 
-from singer_sdk.testing import get_tap_test_class
+from singer_sdk.testing import SuiteConfig, get_tap_test_class
 
 from tap_neon.tap import TapNeon
 
-TestTapNeon = get_tap_test_class(TapNeon, config={})
+TestTapNeon = get_tap_test_class(
+    TapNeon,
+    config={},
+    suite_config=SuiteConfig(max_records_limit=None),
+)
